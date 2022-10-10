7cd37546c6d86120f7a9546e48ea47029def1c89
Raul Ospina <raminka13@hotmail.com>
2022-10-10 14:38:23 -0500

    Create Item and methods

---
+++ item.rb
@@ -0,0 +1,26 @@
+require 'date'
+
+class Item
+    def initialize(genre, publish_date, archived: false)
+        @id = 324
+        @genre = genre
+        @author = 'author'
+        @label = 'label'
+        @publish_date = publish_date
+        @archived = archived
+    end
+
+    def moved_to_archive
+      @archived = can_be_archived?
+    end
+
+    private
+    def can_be_archived?
+      date = DateTime.now.strftime('%Y').to_i
+      publish_year = @publish_date[6..10].to_i
+      date - publish_year > 10
+    end
+end
+
+item = Item.new('Rock', '10/10/2010')
+item.moved_to_archive
