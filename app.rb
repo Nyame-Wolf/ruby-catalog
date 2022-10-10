36ad43a136c572f580a220cc53f99862444e5113
Nyame-Wolf <mumenyam@gmail.com>
2022-10-10 23:41:30 +0300

    fix linter errors

--- app.rb
+++ app.rb
@@ -9,28 +9,28 @@ class App
     when 1
       puts '1 -> List all books'
     when 2
-     puts '2 -> List all music albums'
+      puts '2 -> List all music albums'
     when 3
-     puts '3 -> List all labels'
+      puts '3 -> List all labels'
     when 4
       puts '4 -> List all music albums'
     when 5
-     puts '5 -> List all genres'
+      puts '5 -> List all genres'
     when 6
-     puts '6 -> List all games'
+      puts '6 -> List all games'
     end
   end
 
   def add_options
     case @user_options
     when 7
-     puts '7 -> List all authors'
+      puts '7 -> List all authors'
     when 8
-     puts '8 -> Add book'
+      puts '8 -> Add book'
     when 9
-     puts '9 -> Add music album'
+      puts '9 -> Add music album'
     when 10
-     puts '10 -> Add a game'
+      puts '10 -> Add a game'
     when 11
       exit_app
     else
@@ -50,10 +50,9 @@ class App
     dashboard
   end
 
-    def exit_app
+  def exit_app
     puts 'Thank you for using this app'
     # add save json files
     exit
   end
 end
-
