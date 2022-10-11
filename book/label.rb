59fef785c02e332fc1b54ac3f1c4dd6236d44ad2
Raul Ospina <raminka13@hotmail.com>
2022-10-11 18:06:23 -0500

    Create Label class

---
+++ book/label.rb
@@ -0,0 +1,25 @@
+class Label
+  attr_reader :title, :color, :id
+  attr_accessor :items
+
+  def initialize(title, color, id: nil)
+    @id = id || SecureRandom.random_number(1000)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item unless @items.include?(item)
+    item.label = self unless item.label == self
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => @id,
+      'title' => @title,
+      'color' => @color
+    }.to_json(*args)
+  end
+end
