66048ec73d5a42ddd1caecc3b1811b944eb55c3f
Raul Ospina <raminka13@hotmail.com>
2022-10-11 18:07:55 -0500

    Create module for share data

---
+++ data_values.rb
@@ -0,0 +1,32 @@
+module DataValues
+  @books = []
+  @albums = []
+  @games = []
+  @genres = []
+  @authors = []
+  @labels = []
+
+  def self.books
+    @books
+  end
+
+  def self.albums
+    @albums
+  end
+
+  def self.games
+    @games
+  end
+
+  def self.genres
+    @genres
+  end
+
+  def self.authors
+    @authors
+  end
+
+  def self.labels
+    @labels
+  end
+end
