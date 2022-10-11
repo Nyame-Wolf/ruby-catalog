cb724bb0281b97e83f29ee256133286ad94ae2fa
Raul Ospina <raminka13@hotmail.com>
2022-10-11 18:05:30 -0500

    Add Book Class

---
+++ book/book.rb
@@ -0,0 +1,28 @@
+require_relative '../item'
+
+class Book < Item
+  attr_reader :publisher, :cover_state
+
+  def initialize(publisher, cover_state, id: nil, publish_date: nil)
+    super(id, publish_date)
+    @publisher = publisher
+    @cover_state = cover_state
+  end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'id' => @id,
+      'publish_date' => @publish_date,
+      'publisher' => @publisher,
+      'cover_state' => @cover_state,
+      'label' => @label
+    }.to_json(*args)
+  end
+
+  private
+
+  def can_be_archived?
+    (@cover_state == 'bad' || super)
+  end
+end
