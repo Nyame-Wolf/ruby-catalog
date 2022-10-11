5dbcb5b9a6e27eda091943950aedc518a9b6339f
Nyame-Wolf <mumenyam@gmail.com>
2022-10-11 11:58:25 +0300

    add a music album class and initiaize it

---
+++ music-album/music-album.rb
@@ -0,0 +1,13 @@
+require_relative '../item'
+class MusicAlbum < Item
+  attr_accessor :on_spotify, :name
+
+  def initialize(*args, on_spotify)
+    super(*args)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    super && on_spotify
+  end
+end
