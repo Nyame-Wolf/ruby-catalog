9ea46919ae2ac2aa6953f9faa6fbb5f3b5920f00
Nyame-Wolf <mumenyam@gmail.com>
2022-10-12 22:35:14 +0300

    remove default values for id and just create the id automatically if not given

--- music-album/genre.rb
+++ music-album/genre.rb
@@ -1,8 +1,10 @@
+require_relative '../item'
 require 'securerandom'
 class Genre
   attr_accessor :name, :id, :items
-  def initialize(name, id: nil)
-    @id = id || SecureRandom.random_number(1000)
+
+  def initialize(name)
+    @id = nil || SecureRandom.random_number(1000)
     @name = name
     @items = []
   end
