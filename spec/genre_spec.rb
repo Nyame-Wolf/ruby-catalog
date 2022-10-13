699b718a6f1ac8aaa017e627115cdabb9f809081
Nyame-Wolf <mumenyam@gmail.com>
2022-10-13 16:37:35 +0300

    Add more unit tests to genre and misic album classes

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -2,9 +2,12 @@ require_relative '../music-album/genre'
 
 describe Genre do
   context 'Testing Genre' do
+    genre = Genre.new('Pop')
     it 'it creates a genre instance' do
-      genre = Genre.new('Pop')
       expect(genre.name).to eq('Pop')
+    end
+    it 'is an instance of genre and takes 3 parameter' do
+      expect(genre).to be_instance_of(Genre)
     end
   end
 end
