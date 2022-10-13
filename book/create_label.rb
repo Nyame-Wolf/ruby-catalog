64cf0b9689db3566e63229d8f5f513282fa16577
Raul Ospina <raminka13@hotmail.com>
2022-10-13 09:54:07 -0500

    Fix code for better function

--- book/create_label.rb
+++ book/create_label.rb
@@ -38,7 +38,6 @@ class CreateLabel
   def list_labels
     if @label_list.empty?
       puts 'Please ADD some Labels...'
-      choose_label
     else
       @label_list.each_with_index do |label, index|
         puts "#{index}) Title: #{label.title},  Color: #{label.color}"
